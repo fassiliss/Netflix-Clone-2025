d7b0e90a42d597853594df187ae3018a7d9fc945
fassiliss <fassiliss@gmail.com>
2025-02-11 16:29:48 -0600

    header and footer

--- Netflix-Clone-2025/src/App.jsx
+++ Netflix-Clone-2025/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 
 import './App.css'
+import Home from './pages/Home/Home'
 
 function App() {
   
   return (
     <>
       <div className="App">
-        <h1>Hello</h1>
+        <Home/>
       </div>
       
     </>
