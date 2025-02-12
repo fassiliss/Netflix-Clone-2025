ab3a87831d83f929844173b70a325ca90be31690
fassiliss <fassiliss@gmail.com>
2025-02-12 00:32:50 -0600

    update api key

--- Netflix-Clone-2025/src/utils/requests.jsx
+++ Netflix-Clone-2025/src/utils/requests.jsx
@@ -1,4 +1,4 @@
-const API_KEY = "0979fea3021eba74aecec3f91feddc91"
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
